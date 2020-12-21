7704869705b00019d9b31e99f4b6af7b0103c29a
Daniel R <99danielrod@gmail.com>
2020-12-21 10:40:02 -0600

    directory added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div >
-      hello world
-    </div>
+    <HomePage></HomePage>
   );
 }
 
