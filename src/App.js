b7dab4eefcc86b10c690809af86e212d6425e300
daniel rodriguez <99danielrod@gmail.com>
2021-01-22 18:31:39 -0600

    Sign in components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInUp from './pages/sign-in-up/sign-in-up.component';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}></Route>
         <Route exact path='/shop' component={ShopPage}></Route>
+        <Route exact path='/signIn' component={SignInUp}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-up.styles.scss';
+
+const SignInUp =()=>{
+   return <div className='sign-in-and-sign-up'>
+       <SignIn></SignIn>
+   </div>
+}
+export default SignInUp;
