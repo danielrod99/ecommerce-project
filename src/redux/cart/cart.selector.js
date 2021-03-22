2a2aff9553109e36977a3691e4e0df4e1fdebcbb
daniel rodriguez <99danielrod@gmail.com>
2021-03-22 14:02:11 -0600

    redux selector

---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,13 @@
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+    [selectCart],
+    (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+    [selectCartItems],
+    selectCartItems => selectCartItems.reduce((accum, cartItem) => accum + cartItem.quantity, 0)
+)
