d7d1187236d1c7c5dd5896e80337bd35a752e19f
Daniel Rodriguez <danielrs999@hotmail.com>
2021-04-09 15:02:06 -0500

    checkout page add and remove items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,8 +1,8 @@
 import CartActionTypes from './cart.types'
-import {addItemToCart} from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 const INITIAL_STATE = {
     hidden: true,
-    cartItems:[]
+    cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
@@ -13,9 +13,19 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 hidden: !state.hidden
             }
         case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload),
+            }
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
+        case CartActionTypes.REMOVE_ITEM:
             return{
                 ...state,
-                cartItems:addItemToCart(state.cartItems, action.payload),
+                cartItems: removeItemFromCart(state.cartItems,action.payload)
             }
         default:
             return state;
