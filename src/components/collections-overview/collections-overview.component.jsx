1a3e3ba00a9b0a745a3ff9c32a0759e3c78dc220
Daniel Rodriguez <danielrs999@hotmail.com>
2021-10-15 12:03:24 -0500

    collections fixed

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,15 +3,24 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectShopItems } from '../../redux/shop/shop.selector';
 
-import  PreviewCollection from '../preview-collection/preview-collection.component';
+import PreviewCollection from '../preview-collection/preview-collection.component';
 
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => {
+    if (collections) {
+        var cols = []
+        var keys = Object.keys(collections);
+        for (let i = 0; i < keys.length; i++) {
+            cols.push(collections[keys[i]]);
+        }
+    } else {
+        return <div></div>
+    }
     return (
         <div className='collections-overview'>
             {
-                collections.map(({ id, ...collProps }) => {
+                cols.map(({ id, ...collProps }) => {
                     return <PreviewCollection key={id} {...collProps}></PreviewCollection>
                 })
             }
